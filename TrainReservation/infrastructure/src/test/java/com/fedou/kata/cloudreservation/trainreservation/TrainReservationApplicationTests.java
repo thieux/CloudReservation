73df0bd863c23ea5733b1de293349b7bf335c527
Nicolas Fédou <nicolas.fedou@gmail.com>
2019-08-26 11:50:07 +0200

    Making tests pass again

--- TrainReservation/infrastructure/src/test/java/com/fedou/kata/cloudreservation/trainreservation/TrainReservationApplicationTests.java
+++ TrainReservation/infrastructure/src/test/java/com/fedou/kata/cloudreservation/trainreservation/TrainReservationApplicationTests.java
@@ -2,10 +2,14 @@ package com.fedou.kata.cloudreservation.trainreservation;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fedou.kata.cloudreservation.trainreservation.bookingreference.BookingReferenceRemote;
+import com.fedou.kata.cloudreservation.trainreservation.bookingreference.BookingReferenceService;
 import com.fedou.kata.cloudreservation.trainreservation.traindata.TrainDataService;
+import org.mockito.Mock;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.TestConfiguration;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.boot.test.mock.mockito.SpyBean;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.web.servlet.MockMvc;
@@ -22,10 +26,10 @@ public class TrainReservationApplicationTests {
     @Autowired
     protected ObjectMapper mapper;
 
-    @SpyBean
+    @MockBean
     protected TrainDataService trainDataService;
-    @SpyBean
-    protected BookingReferenceRemote bookingReferenceService;
+    @MockBean
+    protected BookingReferenceService bookingReferenceService;
 
     protected <T> T getResultAsType(MvcResult result, Class<T> valueType) throws java.io.IOException {
         return mapper.readValue(result.getResponse().getContentAsString(), valueType);
